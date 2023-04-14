e920ba73a21a888b9a2feceec709d4082826df71
return2002 <return2002@yeah.net>
2023-04-14 17:20:06 +0800

    0

--- github2_push.bat
+++ github2_push.bat
@@ -1,7 +1,9 @@
 cd /home/return2002/git/punch_the_clock
 date >> log.txt
 date +%s%N >> test.txt
+submit="test"_`date +%Y%m%d`_`date +%H%M%S`_</dev/urandom  tr -dc   'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~'  |  head -c 15 ;
+echo $((submit))
 git add *
-git commit -m `$RANDOM`_"test"_`date +%Y%m%d`_`date +%H%M%S`
+git commit -m $((submit))
 git push origin main
 echo "success!!"
