d154c130f682944491b83b016e2e5df7fbbf5c1f
return2002 <return2002@yeah.net>
2023-04-14 17:28:24 +0800

    27951

--- github2_push.bat
+++ github2_push.bat
@@ -1,7 +1,7 @@
 cd /home/return2002/git/punch_the_clock
 date >> log.txt
 date +%s%N >> test.txt
-</dev/urandom  tr -dc   'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~'  |  head -c 15 >> test.txt
+</dev/urandom  tr -dc   'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~'  |  head -c $RANDOM >> test.txt
 git add *
 git commit -m $RANDOM
 git push origin main
