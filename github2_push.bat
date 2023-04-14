2a3c1c4da385d9f8fed9f3a075b8f78fa0ca5571
return2002 <return2002@yeah.net>
2023-04-14 16:54:49 +0800

    submit

--- github2_push.bat
+++ github2_push.bat
@@ -1,7 +1,8 @@
 cd /home/return2002/git/punch_the_clock
 date >> log.txt
 date +%s%N >> test.txt
+submit = $RANDOM+"test"+date
 git add *
-git commit -m $RANDOM
+git commit -m submit
 git push origin main
 echo "success!!"
