ecfb3f0dce7e5378d9fd39fd604e2eb59cea90ba
return2002 <return2002@yeah.net>
2023-04-14 17:10:42 +0800

    _test_20230414_171042

--- github2_push.bat
+++ github2_push.bat
@@ -2,6 +2,6 @@ cd /home/return2002/git/punch_the_clock
 date >> log.txt
 date +%s%N >> test.txt
 git add *
-git commit -m $RANDOM_"test"_`date +%Y%m%d`_`date +%H%M%S`
+git commit -m `$RANDOM`_"test"_`date +%Y%m%d`_`date +%H%M%S`
 git push origin main
 echo "success!!"
